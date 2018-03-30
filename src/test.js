90466326c7c73a7c7a7f53fe6bf06a8c364bc9eb
Alex Bepple <alex@bepple.de>
2018-03-30 18:07:34 +0200

    $ demonstrate problem

--- src/test.js
+++ src/test.js
@@ -1,7 +1,12 @@
-import {assertThat, is} from 'hamjest'
+import { assertThat, is } from 'hamjest'
+import td from 'testdouble'
 
-describe('foo', () => {
-  it('bar', () => {
-    assertThat(1+1, is(3))
+const fut = (collaborator) => collaborator().substring(1)
+
+describe('Demonstrate problem', () => {
+  it('this fails, but is hard to debug', () => {
+    const stub = td.function()
+    td.when(stub(0)).thenReturn('foo')
+    assertThat(fut(stub), is('oo'))
   })
 })
