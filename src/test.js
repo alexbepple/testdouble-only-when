60f0d1f2d492f480d807118237ae23cada1aaaa4
Alex Bepple <alex@bepple.de>
2018-03-31 01:44:15 +0200

    $ reframe problem as test

--- src/test.js
+++ src/test.js
@@ -1,12 +1,13 @@
-import { assertThat, is } from 'hamjest'
+import { assertThat, is, throws, typedError } from 'hamjest'
 import td from 'testdouble'
 
-const fut = (collaborator) => collaborator().substring(1)
-
-describe('Demonstrate problem', () => {
-  it('this fails, but is hard to debug', () => {
+describe('Problem: this fails, but is hard to debug', () => {
+  it('fails not because of the wrong invocation, but because of the consequences of default stub behavior', () => {
     const stub = td.function()
     td.when(stub(0)).thenReturn('foo')
-    assertThat(fut(stub), is('oo'))
+
+    const fut = (collaborator) => collaborator().substring(1)
+
+    assertThat(() => fut(stub), throws(typedError(TypeError, "Cannot read property 'substring' of undefined")))
   })
 })
