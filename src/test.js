d69232fad083362854121295e5add5a8354339d0
Alex Bepple <alex@bepple.de>
2018-03-31 02:17:33 +0200

    $ fail early on unrehearsed invocation

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-import { assertThat, is, throws, typedError } from 'hamjest'
+import { assertThat, is, not, throws, typedError, instanceOf } from 'hamjest'
 import td from 'testdouble'
 
 describe('Problem: this fails, but is hard to debug', () => {
@@ -9,5 +9,34 @@ describe('Problem: this fails, but is hard to debug', () => {
     const fut = (collaborator) => collaborator().substring(1)
 
     assertThat(() => fut(stub), throws(typedError(TypeError, "Cannot read property 'substring' of undefined")))
+  })
+})
+
+const onlyWhen = (double) => {
+  const shadowDouble = td.function()
+  return {
+    calledWith: (...expectedParams) => ({
+      thenReturn: (...returnValues) => {
+        td.when(shadowDouble(...expectedParams)).thenReturn(...returnValues)
+        td.when(double(), { ignoreExtraArgs: true }).thenDo((...actualParams) => {
+          const fromShadow = shadowDouble(...actualParams)
+          if (fromShadow) return fromShadow
+          throw new Error('You invoked a test double in an unexpected fashion.')
+        })
+      }
+    })
+  }
+}
+
+describe('Solution', () => {
+  const stub = td.function()
+
+  before(() => onlyWhen(stub).calledWith(0).thenReturn(1))
+
+  it('fail early on unrehearsed invocation', () => {
+    assertThat(() => stub(), throws(not(instanceOf(TypeError))))
+  })
+  it('succeed on rehearsed invocation', () => {
+    assertThat(stub(0), is(1))
   })
 })
