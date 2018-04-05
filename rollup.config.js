2b290429a8844b8ec305f20cd698c7136872c939
Alex Bepple <alex@bepple.de>
2018-04-05 09:26:19 +0200

    . avoid Rollup warning
    (!) Unresolved dependencies
    https://github.com/rollup/rollup/wiki/Troubleshooting#treating-module-as-external-dependency
    testdouble/lib/store/stubbings (imported by src/index.js)

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 export default {
   input: 'src/index.js',
-  external: ['testdouble'],
+  external: [
+    'testdouble',
+    'testdouble/lib/store/stubbings'
+  ],
   output: {
     format: 'cjs',
     file: 'dist/bundle.js'
